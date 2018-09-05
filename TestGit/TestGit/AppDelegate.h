c0c8d61ea8f47fb366c07dbe2365408fa16b5e78
RenJie Yan <1282000376@qq.com>
2018-09-05 23:30:05 +0800

    初始化项目

---
+++ TestGit/TestGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestGit
+//
+//  Created by 颜仁浩 on 2018/9/5.
+//  Copyright © 2018年 颜仁浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestGit/TestGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestGit
+//
+//  Created by 颜仁浩 on 2018/9/5.
+//  Copyright © 2018年 颜仁浩. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
